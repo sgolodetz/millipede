9cb3341bfc72f65698b0d140d0f1b251cfd91dd1
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2009-11-16 15:21:28 +0000

    Implemented TextureFactory (or a first version of it, at any rate).
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@114 2d08482b-68a0-452d-bcbc-5be95be64364

--- trunk/source/common/textures/TextureFactory.cpp
+++ trunk/source/common/textures/TextureFactory.cpp
@@ -5,8 +5,23 @@
 
 #include "TextureFactory.h"
 
+#include <common/exceptions/Exception.h>
+#include "Greyscale8ImageTexture.h"
+
 namespace mp {
 
-// TODO
+//#################### PUBLIC METHODS ####################
+Texture_Ptr TextureFactory::create_texture(const Greyscale8ImageCPointer& image, bool clamp)
+{
+	Greyscale8Image::SizeType size = image->GetLargestPossibleRegion().GetSize();
+	check_dimensions(size[0], size[1]);
+	return Texture_Ptr(new Greyscale8ImageTexture(image, clamp));
+}
+
+//#################### PRIVATE METHODS ####################
+void TextureFactory::check_dimensions(int width, int height)
+{
+	if(width < 2 || width > 1024 || height < 2 || height > 1024) throw Exception("Image dimensions out of range");
+}
 
 }
--- trunk/source/common/textures/TextureFactory.h
+++ trunk/source/common/textures/TextureFactory.h
@@ -6,11 +6,30 @@
 #ifndef H_MILLIPEDE_TEXTUREFACTORY
 #define H_MILLIPEDE_TEXTUREFACTORY
 
+#include <boost/shared_ptr.hpp>
+using boost::shared_ptr;
+
+#include <itkImage.h>
+
 namespace mp {
+
+//#################### FORWARD DECLARATIONS ####################
+typedef shared_ptr<class Texture> Texture_Ptr;
 
 class TextureFactory
 {
-	// TODO
+	//#################### TYPEDEFS ####################
+private:
+	typedef itk::Image<unsigned char,2> Greyscale8Image;
+	typedef itk::Image<unsigned char,2>::ConstPointer Greyscale8ImageCPointer;
+
+	//#################### PUBLIC METHODS ####################
+public:
+	static Texture_Ptr create_texture(const Greyscale8ImageCPointer& image, bool clamp = true);
+
+	//#################### PRIVATE METHODS ####################
+private:
+	static void check_dimensions(int width, int height);
 };
 
 }
