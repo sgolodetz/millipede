6c5d8db60b82e3f65cc57a6919cfb4624d50598f
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-05-11 17:19:17 +0000

    Migrated the command management system across from the partition forest testbed (with minor changes).
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@147 2d08482b-68a0-452d-bcbc-5be95be64364

---
+++ trunk/source/common/commands/ICommandManager.h
@@ -0,0 +1,38 @@
+/***
+ * millipede: ICommandManager.h
+ * Copyright Stuart Golodetz, 2010. All rights reserved.
+ ***/
+
+#ifndef H_MILLIPEDE_ICOMMANDMANAGER
+#define H_MILLIPEDE_ICOMMANDMANAGER
+
+#include <boost/shared_ptr.hpp>
+
+namespace mp {
+
+//#################### FORWARD DECLARATIONS ####################
+typedef boost::shared_ptr<class Command> Command_Ptr;
+
+class ICommandManager
+{
+	//#################### DESTRUCTOR ####################
+public:
+	virtual ~ICommandManager() {}
+
+	//#################### PUBLIC ABSTRACT METHODS ####################
+public:
+	virtual void begin_command_sequence() = 0;
+	virtual bool can_redo() const = 0;
+	virtual bool can_undo() const = 0;
+	virtual void clear_history() = 0;
+	virtual void end_command_sequence(const std::string& description) = 0;
+	virtual void execute(const Command_Ptr& command) = 0;
+	virtual void redo() = 0;
+	virtual std::string redo_description() const = 0;
+	virtual void undo() = 0;
+	virtual std::string undo_description() const = 0;
+};
+
+}
+
+#endif
---
+++ trunk/source/common/commands/UndoableCommandManager.cpp
@@ -0,0 +1,97 @@
+/***
+ * millipede: UndoableCommandManager.cpp
+ * Copyright Stuart Golodetz, 2010. All rights reserved.
+ ***/
+
+#include "UndoableCommandManager.h"
+
+#include <deque>
+
+#include <common/exceptions/Exception.h>
+#include "SequenceCommand.h"
+
+namespace mp {
+
+//#################### LOCAL CLASSES ####################
+class UndoableCommandManager::MarkerCommand : public Command
+{
+public:
+	MarkerCommand() : Command("Marker") {}
+	void execute() {}
+	void undo() {}
+};
+
+//#################### CONSTRUCTORS ####################
+UndoableCommandManager::UndoableCommandManager()
+:	m_markerCommand(new MarkerCommand)
+{}
+
+//#################### PUBLIC METHODS ####################
+void UndoableCommandManager::begin_command_sequence()
+{
+	execute(m_markerCommand);
+}
+
+bool UndoableCommandManager::can_redo() const
+{
+	return !m_undone.empty();
+}
+
+bool UndoableCommandManager::can_undo() const
+{
+	return !m_done.empty();
+}
+
+void UndoableCommandManager::clear_history()
+{
+	m_done.clear();
+	m_undone.clear();
+}
+
+// Precondition: begin_command_sequence() has been called since end_command_sequence() was last called
+void UndoableCommandManager::end_command_sequence(const std::string& description)
+{
+	std::deque<Command_Ptr> sequence;
+	for(;;)
+	{
+		if(m_done.empty()) throw Exception("No command sequence had been started");
+
+		Command_Ptr latest = m_done.back();
+		m_done.pop_back();
+
+		if(latest != m_markerCommand) sequence.push_front(latest);
+		else break;
+	}
+	m_done.push_back(Command_Ptr(new SequenceCommand(description, sequence)));
+}
+
+void UndoableCommandManager::execute(const Command_Ptr& command)
+{
+	command->execute();
+	m_done.push_back(command);
+	m_undone.clear();
+}
+
+void UndoableCommandManager::redo()
+{
+	if(can_redo())
+	{
+		Command_Ptr command = m_undone.back();
+		m_undone.pop_back();
+		command->redo();
+		m_done.push_back(command);
+	}
+}
+
+void UndoableCommandManager::undo()
+{
+	if(can_undo())
+	{
+		Command_Ptr command = m_done.back();
+		m_done.pop_back();
+		command->undo();
+		m_undone.push_back(command);
+	}
+}
+
+}
