46e9677c12ccb4d428080dcfcf7f8a14f019b537
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-06-24 15:29:31 +0000

    Added a threads test to test-boost_1_39_0.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@383 2d08482b-68a0-452d-bcbc-5be95be64364

--- trunk/source/tests/test-boost_1_39_0/main.cpp
+++ trunk/source/tests/test-boost_1_39_0/main.cpp
@@ -6,12 +6,52 @@
 #include <iostream>
 
 #include <boost/shared_ptr.hpp>
-using boost::shared_ptr;
+#include <boost/thread/condition.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
 
+//#################### SHARED_PTR TEST ####################
+void test_shared_ptr()
+{
+	boost::shared_ptr<int> p(new int(23));
+	boost::shared_ptr<int> q = p;
+	std::cout << *q << std::endl;
+}
+
+//#################### THREADS TEST ####################
+boost::mutex mut;
+boost::condition cond;
+bool ready = false;
+
+void thread_a()
+{
+	for(int i=0; i<5; ++i) std::cout << "In Thread A\n";
+
+	boost::mutex::scoped_lock lock(mut);
+	ready = true;
+	cond.notify_one();
+}
+
+void thread_b()
+{
+	boost::mutex::scoped_lock lock(mut);
+	if(!ready) cond.wait(lock);
+
+	for(int i=0; i<5; ++i) std::cout << "In Thread B\n";
+}
+
+void test_threads()
+{
+	boost::thread thA(thread_a);
+	boost::thread thB(thread_b);
+	thA.join();
+	thB.join();
+}
+
+//#################### MAIN FUNCTION ####################
 int main()
 {
-	shared_ptr<int> p(new int(23));
-	shared_ptr<int> q = p;
-	std::cout << *q << std::endl;
+	test_shared_ptr();
+	test_threads();
 	return 0;
 }
