6626b111bc53a12dea0ea69dc95a117e7472a124
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-06-30 18:30:01 +0000

    Introduced SegmentVolumeDialog, and started extracting common functionality from SegmentCTVolumeDialog up into it.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@410 2d08482b-68a0-452d-bcbc-5be95be64364

--- branches/20100630-mri/source/apps/mast/gui/dialogs/SegmentCTVolumeDialog.cpp
+++ branches/20100630-mri/source/apps/mast/gui/dialogs/SegmentCTVolumeDialog.cpp
@@ -42,26 +42,9 @@ namespace mp {
 
 //#################### CONSTRUCTORS ####################
 SegmentCTVolumeDialog::SegmentCTVolumeDialog(wxWindow *parent, const itk::Size<3>& volumeSize, const WindowSettings& windowSettings)
-:	m_volumeSize(volumeSize), m_windowSettings(windowSettings)
+:	SegmentVolumeDialog<CTSegmentationOptions>(parent, volumeSize, windowSettings)
 {
-	Create(parent, wxID_ANY, wxT("Segment CT Volume"), wxDefaultPosition, wxDefaultSize);
-
-	wxBookCtrlBase *notebook = GetBookCtrl();
-	wxPanel *basicPage = create_basic_page(notebook);
-	wxPanel *advancedPage = create_advanced_page(notebook);
-	wxPanel *modalityPage = create_modality_page(notebook);
-	notebook->AddPage(basicPage, wxT("Basic"), true);
-	notebook->AddPage(advancedPage, wxT("Advanced"), false);
-	notebook->AddPage(modalityPage, wxT("Modality-Specific"), false);
-
-	CreateButtons();
-	LayoutDialog();
-}
-
-//#################### PUBLIC METHODS ####################
-const boost::optional<CTSegmentationOptions>& SegmentCTVolumeDialog::segmentation_options() const
-{
-	return m_segmentationOptions;
+	initialise(parent);
 }
 
 //#################### PRIVATE METHODS ####################
@@ -83,79 +66,6 @@ bool SegmentCTVolumeDialog::construct_segmentation_options()
 	CTSegmentationOptions::InputType inputType = CTSegmentationOptions::InputType(m_inputType->GetSelection());
 	m_segmentationOptions = CTSegmentationOptions(adfIterations, subvolumeSize, waterfallLayerLimit, m_windowSettings, inputType);
 	return true;
-}
-
-wxPanel *SegmentCTVolumeDialog::create_advanced_page(wxWindow *parent)
-{
-	wxPanel *panel = new wxPanel(parent);
-
-	wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-	panel->SetSizer(sizer);
-
-	// Set up the spin control to select the number of anisotropic diffusion filtering iterations.
-	wxPanel *filteringPanel = new wxPanel(panel);
-	sizer->Add(filteringPanel);
-	wxGridSizer *filteringSizer = new wxGridSizer(0, 2, 0, 0);
-	filteringPanel->SetSizer(filteringSizer);
-
-	filteringSizer->Add(new wxStaticText(filteringPanel, wxID_ANY, wxT("ADF Iterations:")));
-	m_adfIterations = new wxSpinCtrl(filteringPanel, wxID_ANY, wxT("20"), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 30, 20);
-	filteringSizer->Add(m_adfIterations);
-
-	// Set up the spin control to select a waterfall layer limit.
-	wxPanel *waterfallPanel = new wxPanel(panel);
-	sizer->Add(waterfallPanel);
-	wxGridSizer *waterfallSizer = new wxGridSizer(0, 2, 0, 0);
-	waterfallPanel->SetSizer(waterfallSizer);
-
-	waterfallSizer->Add(new wxStaticText(waterfallPanel, wxID_ANY, wxT("Waterfall Layer Limit:")));
-	m_waterfallLayerLimit = new wxSpinCtrl(waterfallPanel, wxID_ANY, wxT("5"), wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 0, 10, 5);
-	waterfallSizer->Add(m_waterfallLayerLimit);
-
-	sizer->Fit(panel);
-	return panel;
-}
-
-wxPanel *SegmentCTVolumeDialog::create_basic_page(wxWindow *parent)
-{
-	wxPanel *panel = new wxPanel(parent);
-
-	wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-	panel->SetSizer(sizer);
-
-	// Set up the radio box to choose a segmentation type.
-	wxString strings[SEGTYPE_COUNT];
-	strings[SEGTYPE_XY] = wxT("Segment as &Axial (X-Y) Slices");
-	strings[SEGTYPE_XZ] = wxT("Segment as &Coronal (X-Z) Slices");
-	strings[SEGTYPE_YZ] = wxT("Segment as &Sagittal (Y-Z) Slices");
-	strings[SEGTYPE_3D] = wxT("Segment as 3D &Volume");
-	strings[SEGTYPE_CUSTOM] = wxT("Segment using C&ustomised Sub-Volume Size");
-	m_segmentationType = new wxRadioBox(panel, RADIOBOXID_SEGMENTATIONTYPE, wxT("Segmentation Type"), wxDefaultPosition, wxDefaultSize, SEGTYPE_COUNT, strings, 1, wxRA_SPECIFY_COLS);
-	sizer->Add(m_segmentationType, 0, wxALIGN_CENTER_HORIZONTAL);
-
-	sizer->AddSpacer(10);
-
-	// Set up the subvolume size options.
-	wxStaticBoxSizer *subvolumeSizeOptions = new wxStaticBoxSizer(wxVERTICAL, panel, wxT("Sub-Volume Size"));
-	sizer->Add(subvolumeSizeOptions);
-
-	wxPanel *subvolumePanel = new wxPanel(panel);
-	subvolumeSizeOptions->Add(subvolumePanel);
-	wxGridSizer *subvolumeSizer = new wxGridSizer(0, 2, 0, 0);
-	subvolumePanel->SetSizer(subvolumeSizer);
-
-	wxString captions[] = {"X Size:", "Y Size:", "Z Size:"};
-	for(int i=0; i<3; ++i)
-	{
-		subvolumeSizer->Add(new wxStaticText(subvolumePanel, wxID_ANY, captions[i]));
-		int initial = i != 2 ? m_volumeSize[i] : 1;
-		wxString initialValue = string_to_wxString(boost::lexical_cast<std::string>(initial));
-		m_subvolumeSizes[i] = new wxSpinCtrl(subvolumePanel, SPINID_GRIDSIZE, initialValue, wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS, 1, m_volumeSize[i], initial);
-		subvolumeSizer->Add(m_subvolumeSizes[i]);
-	}
-
-	sizer->Fit(panel);
-	return panel;
 }
 
 wxPanel *SegmentCTVolumeDialog::create_modality_page(wxWindow *parent)
