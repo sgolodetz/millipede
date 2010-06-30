cc8235c8c10c9e4a2754cde4fe8e2756eed39097
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-06-30 21:34:27 +0000

    Implemented a first attempt at MRLowestLayersBuilder (it currently looks a lot like CTLowestLayersBuilder, but that will ultimately change).
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@420 2d08482b-68a0-452d-bcbc-5be95be64364

--- branches/20100630-mri/source/common/segmentation/MRLowestLayersBuilder.h
+++ branches/20100630-mri/source/common/segmentation/MRLowestLayersBuilder.h
@@ -1,24 +1,24 @@
 /***
- * millipede: CTLowestLayersBuilder.h
+ * millipede: MRLowestLayersBuilder.h
  * Copyright Stuart Golodetz, 2010. All rights reserved.
  ***/
 
-#ifndef H_MILLIPEDE_CTLOWESTLAYERSBUILDER
-#define H_MILLIPEDE_CTLOWESTLAYERSBUILDER
+#ifndef H_MILLIPEDE_MRLOWESTLAYERSBUILDER
+#define H_MILLIPEDE_MRLOWESTLAYERSBUILDER
 
 #include <common/jobs/DataHook.h>
 #include <common/jobs/SimpleJob.h>
 #include <common/partitionforests/base/PartitionForest.h>
 #include <common/partitionforests/images/CTMRImageBranchLayer.h>
 #include <common/partitionforests/images/CTMRImageLeafLayer.h>
-#include "CTSegmentationOptions.h"
+#include "MRSegmentationOptions.h"
 
 namespace mp {
 
 //#################### FORWARD DECLARATIONS ####################
 typedef boost::shared_ptr<const class DICOMVolume> DICOMVolume_CPtr;
 
-class CTLowestLayersBuilder : public SimpleJob
+class MRLowestLayersBuilder : public SimpleJob
 {
 	//#################### TYPEDEFS ####################
 public:
@@ -26,18 +26,18 @@ public:
 	typedef boost::shared_ptr<CTMRImageLeafLayer> CTMRImageLeafLayer_Ptr;
 	typedef PartitionForest<CTMRImageLeafLayer,CTMRImageBranchLayer> IPF;
 	typedef boost::shared_ptr<IPF> IPF_Ptr;
-	typedef CTSegmentationOptions SegmentationOptions;
+	typedef MRSegmentationOptions SegmentationOptions;
 
 	//#################### PRIVATE VARIABLES ####################
 private:
 	CTMRImageLeafLayer_Ptr& m_leafLayer;
 	CTMRImageBranchLayer_Ptr& m_lowestBranchLayer;
-	CTSegmentationOptions m_segmentationOptions;
+	MRSegmentationOptions m_segmentationOptions;
 	DataHook<DICOMVolume_CPtr> m_volumeHook;
 
 	//#################### CONSTRUCTORS ####################
 public:
-	CTLowestLayersBuilder(const CTSegmentationOptions& segmentationOptions, CTMRImageLeafLayer_Ptr& leafLayer, CTMRImageBranchLayer_Ptr& lowestBranchLayer);
+	MRLowestLayersBuilder(const MRSegmentationOptions& segmentationOptions, CTMRImageLeafLayer_Ptr& leafLayer, CTMRImageBranchLayer_Ptr& lowestBranchLayer);
 
 	//#################### PUBLIC METHODS ####################
 public:
