51eb1c298d4d429891f06f496aafd8c7222a550a
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-06-30 19:52:24 +0000

    Added the MRImageBranchLayer typedef.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@416 2d08482b-68a0-452d-bcbc-5be95be64364

---
+++ branches/20100630-mri/source/common/partitionforests/images/MRImageBranchLayer.h
@@ -0,0 +1,18 @@
+/***
+ * millipede: MRImageBranchLayer.h
+ * Copyright Stuart Golodetz, 2010. All rights reserved.
+ ***/
+
+#ifndef H_MILLIPEDE_MRIMAGEBRANCHLAYER
+#define H_MILLIPEDE_MRIMAGEBRANCHLAYER
+
+#include "ImageBranchLayer.h"
+#include "MRRegionProperties.h"
+
+namespace mp {
+
+typedef ImageBranchLayer<MRRegionProperties> MRImageBranchLayer;
+
+}
+
+#endif
