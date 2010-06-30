2429024eb31d37ae4c0335ffb433cbaefc91718e
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-06-30 23:28:50 +0000

    Removed the now obsolete 20100630-mri branch.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@424 2d08482b-68a0-452d-bcbc-5be95be64364

--- branches/20100630-mri/source/LinkBoost.cmake
+++
@@ -1,9 +0,0 @@
-###################
-# LinkBoost.cmake #
-###################
-
-TARGET_LINK_LIBRARIES(${targetname} ${Boost_LIBRARIES})
-
-IF(NOT MSVC_IDE)
-	TARGET_LINK_LIBRARIES(${targetname} pthread)
-ENDIF(NOT MSVC_IDE)
