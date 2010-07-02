e7730636a1e4e5dd6e15ef9c24dc047e5bef5fda
chris <chris@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-07-02 13:08:51 +0000

    Started work on MultilayerNodeScorer.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@430 2d08482b-68a0-452d-bcbc-5be95be64364

---
+++ branches/20100702-multilayerfeatureid/source/common/featureid/multilayer/MultilayerFeatureIdentifier.h
@@ -0,0 +1,18 @@
+/***
+ * millipede: MultilayerFeatureIdentifier.h
+ * Copyright Stuart Golodetz & Chris Nicholls, 2010. All rights reserved.
+ ***/
+
+#ifndef H_MILLIPEDE_MULTILAYERFEATUREIDENTIFIER
+#define H_MILLIPEDE_MULTILAYERFEATUREIDENTIFIER
+
+namespace mp {
+
+class MultilayerFeatureIdentifier
+{
+	// TODO
+};
+
+}
+
+#endif
---
+++ branches/20100702-multilayerfeatureid/source/common/featureid/multilayer/MultilayerNodeScorer.h
@@ -0,0 +1,125 @@
+/***
+ * millipede: MultilayerNodeScorer.h
+ * Copyright Stuart Golodetz & Chris Nicholls, 2010. All rights reserved.
+ ***/
+
+#ifndef H_MILLIPEDE_MULTILAYERNODESCORER
+#define H_MILLIPEDE_MULTILAYERNODESCORER
+
+#include <cassert>
+#include <map>
+
+#include <boost/shared_ptr.hpp>
+
+#include <common/partitionforests/base/PartitionForest.h>
+#include <common/partitionforests/base/PFNodeID.h>
+
+namespace mp {
+
+template <typename LeafLayer, typename BranchLayer>
+class MultilayerNodeScorer
+{
+	//#################### TYPEDEFS ####################
+private:
+	typedef PartitionForest<LeafLayer,BranchLayer> PartitionForestT;
+	typedef boost::shared_ptr<const PartitionForestT> PartitionForest_CPtr;
+	typedef std::map<PFNodeID,double> Scores;
+
+	//#################### PRIVATE VARIABLES ####################
+private:
+	PartitionForest_CPtr m_forest;
+	Scores m_scores;
+
+	//#################### CONSTRUCTORS ####################
+public:
+	explicit MultilayerNodeScorer(const PartitionForest_CPtr& forest)
+	:	m_forest(forest)
+	{}
+
+	//#################### DESTRUCTOR ####################
+public:
+	virtual ~MultilayerNodeScorer()
+	{}
+
+	//#################### PRIVATE ABSTRACT METHODS ####################
+private:
+	virtual double calculate_score(const PFNodeID& node) const = 0;
+
+	//#################### PUBLIC METHODS ####################
+public:
+	void iterate()
+	{
+		int highestLayer = m_forest->highest_layer();
+		if(highestLayer == 0) return;	// it's possible to have a forest with only a leaf layer
+
+		typedef typename BranchLayer::BranchNodeConstIterator Iter;
+		for(Iter it=m_forest->branch_nodes_cbegin(highestLayer), iend=m_forest->branch_nodes_cend(highestLayer); it!=iend; ++it)
+		{
+			propagate_scores_up(*it);
+			propagate_scores_down(*it, m_scores[*it]);
+		}
+	}
+
+	const Scores& scores() const
+	{
+		return m_scores;
+	}
+	
+	//#################### PRIVATE METHODS ####################
+private:
+	void propagate_scores_down(const PFNodeID& cur, double parentScore)
+	{
+#if 0
+		double thisProbability,probability;
+				thisProbability = probabilities[region];
+			probability		= pow(parentProbability * thisProbability,0.5);
+			std::list<RegionLocator> children = (*m_tree).children_of(region);
+			if (children.empty())
+			{
+				m_probabilities_down[region]=probability;
+			}
+			else
+			{
+				for (std::list<RegionLocator>::const_iterator it=children.begin() ; it != children.end(); it++ )
+				{	
+					filter_down(*it,probability,probabilities);
+				}
+			}
+#endif
+
+		// TODO
+	}
+
+	double propagate_scores_up(const PFNodeID& cur)
+	{
+		double result;
+		if(cur.layer() == 1)
+		{
+			// If the current node's in the lowest branch layer, calculate its score explicitly.
+			result = calculate_score(cur);
+		}
+		else
+		{
+			// Otherwise, calculate the scores of its children recursively, and propagate them upwards.
+			std::set<PFNodeID> children = m_forest->children_of(cur);
+			assert(!children.empty());	// a branch node with no children would be invalid
+			int areaSum = 0;
+			double weightedScoreSum = 0;
+			for(std::set<PFNodeID>::const_iterator it=children.begin(), iend=children.end(); it!=iend; ++it)
+			{
+				int area = m_forest->branch_properties(*it).area();
+				double score = propagate_scores_up(*it);
+				areaSum += area;
+				weightedScoreSum += area * score;
+			}
+			result = weightedScoreSum / areaSum;
+		}
+
+		m_scores[cur] = result;
+		return result;
+	}
+};
+
+}
+
+#endif
