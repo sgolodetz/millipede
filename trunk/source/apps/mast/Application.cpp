6ecf17d979c984a394e2e17455f1b2b8921d07fb
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-07-11 16:47:44 +0000

    Added the platform-specific code necessary to make mast and test-wxWidgets-2.8.10 into foreground applications on Mac OS X.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@539 2d08482b-68a0-452d-bcbc-5be95be64364

--- trunk/source/apps/mast/Application.cpp
+++ trunk/source/apps/mast/Application.cpp
@@ -5,6 +5,10 @@
 
 #include "Application.h"
 
+#ifdef __WXMAC__
+	#include <ApplicationServices/ApplicationServices.h>
+#endif
+
 #include <mast/gui/windows/MainWindow.h>
 
 namespace mp {
@@ -12,6 +16,14 @@ namespace mp {
 //#################### EVENT HANDLERS ####################
 bool Application::OnInit()
 {
+#ifdef __WXMAC__
+	// Make mast into a foreground application on Mac OS X and bring it to the front.
+	ProcessSerialNumber PSN;
+	GetCurrentProcess(&PSN);
+	TransformProcessType(&PSN, kProcessTransformToForegroundApplication);
+	SetFrontProcess(&PSN);
+#endif
+
 	MainWindow *window = new MainWindow("MAST - Millipede Automatic Segmentation Tool");
 	window->Show();
 	return true;
