4d0bc7c11f000aff88a9e676ab11b43449e02076
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-07-17 13:11:22 +0000

    Got rid of GLEW again - I couldn't make it work on Mac OS X.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@597 2d08482b-68a0-452d-bcbc-5be95be64364

--- trunk/source/LinkGLEW.cmake
+++
@@ -1,9 +0,0 @@
-##################
-# LinkGLEW.cmake #
-##################
-
-IF(MSVC_IDE)
-	TARGET_LINK_LIBRARIES(${targetname} glew32)
-ELSE(MSVC_IDE)
-	TARGET_LINK_LIBRARIES(${targetname} GLEW)
-ENDIF(MSVC_IDE)
--- trunk/source/UseGLEW.cmake
+++
@@ -1,6 +0,0 @@
-#################
-# UseGLEW.cmake #
-#################
-
-INCLUDE_DIRECTORIES(${millipede_SOURCE_DIR}/../libraries/glew-1.5.5/include)
-LINK_DIRECTORIES(${millipede_SOURCE_DIR}/../libraries/glew-1.5.5/lib)
