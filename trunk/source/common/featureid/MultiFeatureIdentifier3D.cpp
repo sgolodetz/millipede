ad6daba9fd227b5fbed033c7648f8afcfd76f1dc
sgolodetz <sgolodetz@2d08482b-68a0-452d-bcbc-5be95be64364>
2010-08-22 20:05:34 +0000

    Started work on spleen identification, and lowered the volume threshold for the spinal cord (it was too high).
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/millipede@838 2d08482b-68a0-452d-bcbc-5be95be64364

--- trunk/source/common/featureid/MultiFeatureIdentifier3D.cpp
+++ trunk/source/common/featureid/MultiFeatureIdentifier3D.cpp
@@ -11,6 +11,7 @@
 #include "RibsIdentifier3D.h"
 #include "SpinalCordIdentifier3D.h"
 #include "SpineIdentifier3D.h"
+#include "SpleenIdentifier3D.h"
 
 namespace mp {
 
@@ -18,6 +19,7 @@ namespace mp {
 MultiFeatureIdentifier3D::MultiFeatureIdentifier3D(const DICOMVolume_CPtr& dicomVolume, const VolumeIPF_Ptr& volumeIPF)
 :	FeatureIdentifier(dicomVolume, volumeIPF)
 {
+	// Identify the spine, spinal cord and ribs (these can be used as references).
 	SpineIdentifier3D *spineIdentifier = new SpineIdentifier3D(dicomVolume, volumeIPF);
 	spineIdentifier->set_mfs_hook(get_mfs_hook());
 	add_subjob(spineIdentifier);
@@ -30,6 +32,12 @@ MultiFeatureIdentifier3D::MultiFeatureIdentifier3D(const DICOMVolume_CPtr& dicom
 	ribsIdentifier->set_mfs_hook(get_mfs_hook());
 	add_subjob(ribsIdentifier);
 
+	// Identify major blood vessels like the aorta.
+	AortaIdentifier3D *aortaIdentifier = new AortaIdentifier3D(dicomVolume, volumeIPF);
+	aortaIdentifier->set_mfs_hook(get_mfs_hook());
+	add_subjob(aortaIdentifier);
+
+	// Identify soft tissue organs.
 	LiverIdentifier3D *liverIdentifier = new LiverIdentifier3D(dicomVolume, volumeIPF);
 	liverIdentifier->set_mfs_hook(get_mfs_hook());
 	add_subjob(liverIdentifier);
@@ -38,9 +46,9 @@ MultiFeatureIdentifier3D::MultiFeatureIdentifier3D(const DICOMVolume_CPtr& dicom
 	kidneysIdentifier->set_mfs_hook(get_mfs_hook());
 	add_subjob(kidneysIdentifier);
 
-	AortaIdentifier3D *aortaIdentifier = new AortaIdentifier3D(dicomVolume, volumeIPF);
-	aortaIdentifier->set_mfs_hook(get_mfs_hook());
-	add_subjob(aortaIdentifier);
+	SpleenIdentifier3D *spleenIdentifier = new SpleenIdentifier3D(dicomVolume, volumeIPF);
+	spleenIdentifier->set_mfs_hook(get_mfs_hook());
+	add_subjob(spleenIdentifier);
 }
 
 }
