a80a57aabbaf5e03437a30c5d2fc78e555ad2e99
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-12-26 17:49:25 +0000

    Make CMake pick up wxWidgets automatically.

--- cmake/UseWxWidgets.cmake
+++ cmake/UseWxWidgets.cmake
@@ -2,6 +2,13 @@
 # UseWxWidgets.cmake #
 ######################
 
+SET(wxWidgets_DIR "${PROJECT_SOURCE_DIR}/libraries/wxWidgets-3.1.0" CACHE PATH "")
+SET(wxWidgets_ROOT_DIR ${wxWidgets_DIR})
+SET(WX_ROOT_DIR ${wxWidgets_DIR})
+
+SET(wxWidgets_LIB_DIR "${PROJECT_SOURCE_DIR}/libraries/wxWidgets-3.1.0/lib/vc_lib" CACHE PATH "")
+SET(WX_LIB_DIR ${wxWidgets_LIB_DIR})
+
 FIND_PACKAGE(wxWidgets REQUIRED COMPONENTS adv gl html core base)
 
 IF(wxWidgets_FOUND)
