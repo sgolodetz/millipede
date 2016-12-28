b9cc9f4bf2261a7dd749dbfe4b935f907b015b86
Stuart Golodetz <sgolodetz@gxstudios.net>
2016-12-28 10:53:02 +0000

    Make test-gdcm-1.2.5 into a scratch test.

--- tests/scratch/gdcm/main.cpp
+++ tests/scratch/gdcm/main.cpp
@@ -1,21 +1,21 @@
 /***
- * test-gdcm-1.2.5: main.cpp
+ * scratchtest_gdcm: main.cpp
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
 #include <cstdlib>
 #include <iostream>
 
-#include <gdcmDicomDir.h>
-#include <gdcmDicomDirImage.h>
-#include <gdcmDicomDirPatient.h>
-#include <gdcmDicomDirSerie.h>
-#include <gdcmDicomDirStudy.h>
+#include <gdcm-1.2/gdcmDicomDir.h>
+#include <gdcm-1.2/gdcmDicomDirImage.h>
+#include <gdcm-1.2/gdcmDicomDirPatient.h>
+#include <gdcm-1.2/gdcmDicomDirSerie.h>
+#include <gdcm-1.2/gdcmDicomDirStudy.h>
 
 int main()
 {
 	// Load in the example DICOMDIR.
-	gdcm::DicomDir dicomdir;
+	gdcm1::DicomDir dicomdir;
 	dicomdir.SetFileName("../resources/DICOMDIR");
 	if(dicomdir.Load())
 	{
@@ -24,25 +24,25 @@ int main()
 	else return EXIT_FAILURE;
 
 	// Output the patients.
-	for(gdcm::DicomDirPatient *patient=dicomdir.GetFirstPatient(); patient!=NULL; patient=dicomdir.GetNextPatient())
+	for(gdcm1::DicomDirPatient *patient=dicomdir.GetFirstPatient(); patient!=NULL; patient=dicomdir.GetNextPatient())
 	{
 		// Output the patient's name.
 		std::cout << patient->GetEntryValue(0x0010, 0x0010) << '\n';
 
 		// Output the studies.
-		for(gdcm::DicomDirStudy *study=patient->GetFirstStudy(); study!=NULL; study=patient->GetNextStudy())
+		for(gdcm1::DicomDirStudy *study=patient->GetFirstStudy(); study!=NULL; study=patient->GetNextStudy())
 		{
 			// Output the study's description.
 			std::cout << '\t' << study->GetEntryValue(0x0008, 0x1030) << '\n';
 
 			// Output the series.
-			for(gdcm::DicomDirSerie *serie=study->GetFirstSerie(); serie!=NULL; serie=study->GetNextSerie())
+			for(gdcm1::DicomDirSerie *serie=study->GetFirstSerie(); serie!=NULL; serie=study->GetNextSerie())
 			{
 				// Output the serie's number and image count.
 				std::cout << "\t\t" << serie->GetEntryValue(0x0020, 0x0011) << ": ";
 
 				int imageCount = 0;
-				for(gdcm::DicomDirImage *image=serie->GetFirstImage(); image!=NULL; image=serie->GetNextImage())
+				for(gdcm1::DicomDirImage *image=serie->GetFirstImage(); image!=NULL; image=serie->GetNextImage())
 				{
 					++imageCount;
 				}
